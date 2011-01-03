ffc2705bc45717d943684efa12335cdeb3d06fc0
Éric Lemoine <eric.lemoine@gmail.com>
2011-01-03 06:37:55 +0100

    setup.py - add a project url, and make geojson our dep

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ version = '0.1'
 setup(name='papyrus',
       version=version,
       description="Geospatial Extensions for Pyramid",
-      long_description="""\
-""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='FOSS4G, Pylons, Pyramid',
       author='\xc3\x89ric Lemoine',
       author_email='eric.lemoine@gmail.com',
-      url='',
+      url='https://github.com/elemoine/papyrus',
       license='BSD',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          'geojson>=1.0,<=1.0.99'
       ],
       entry_points="""
       # -*- Entry points: -*-
+      """,
+      long_description="""\
       """,
       )
