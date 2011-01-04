cb89d7da82ff5119b73876e0e43f8014a91f5cd8
Éric Lemoine <eric.lemoine@gmail.com>
2011-01-04 07:06:51 +0100

    add a geojson renderer factory

---
+++ papyrus/renderers.py
@@ -0,0 +1,10 @@
+import geojson
+
+def geojson_renderer_factory(info):
+    def _render(value, system):
+        request = system.get('request')
+        if request is not None:
+            if not hasattr(request, 'response_content_type'):
+                request.response_content_type = 'application/json'
+        return geojson.dumps(value)
+    return _render
