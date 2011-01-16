d89cddb55fdcf12f0dfa72cebb1e2203ed3e1e91
Éric Lemoine <eric.lemoine@gmail.com>
2011-01-16 21:57:18 +0100

    geojson rendered now fully covered by tests

--- papyrus/tests/test_renderers.py
+++ papyrus/tests/test_renderers.py
@@ -20,5 +20,21 @@ class Test_geojson_renderer_factory(unittest.TestCase):
             'geometry': {'type': 'Point', 'coordinates': [53, -4]},
             'properties': {'title': 'Dict 1'},
             }
-        result = renderer(f, {})
+        request = testing.DummyRequest()
+        result = renderer(f, {'request': request})
         self.assertEqual(result, '{"geometry": {"type": "Point", "coordinates": [53, -4]}, "type": "Feature", "properties": {"title": "Dict 1"}, "id": 1}')
+        self.assertEqual(request.response_content_type, 'application/json')
+
+    def test_geojson_content_type(self):
+        renderer = self._callFUT(None)
+        f = {
+            'type': 'Feature',
+            'id': 1,
+            'geometry': {'type': 'Point', 'coordinates': [53, -4]},
+            'properties': {'title': 'Dict 1'},
+            }
+        request = testing.DummyRequest()
+        request.response_content_type = 'text/javascript'
+        result = renderer(f, {'request': request})
+        self.assertEqual(result, '{"geometry": {"type": "Point", "coordinates": [53, -4]}, "type": "Feature", "properties": {"title": "Dict 1"}, "id": 1}')
+        self.assertEqual(request.response_content_type, 'text/javascript')
