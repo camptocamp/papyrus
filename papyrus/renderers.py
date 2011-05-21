274dd337f05faad01f4568ffbe6315532115d2a5
Éric Lemoine <eric.lemoine@gmail.com>
2011-05-21 21:21:44 +0200

    add a JSON encoder, to deal with decimal.Decimal, datetime.date and datetime.datetime

--- papyrus/renderers.py
+++ papyrus/renderers.py
@@ -1,4 +1,19 @@
+import decimal
+import datetime
+
 import geojson
+from geojson.codec import PyGFPEncoder as GeoJSONEncoder
+
+class Encoder(GeoJSONEncoder):
+    # SQLAlchemy's Reflecting Tables mechanism uses decimal.Decimal
+    # for numeric columns and datetime.date for dates. simplejson
+    # does'nt deal with these types. This class provides a simple
+    # encoder to deal with objects of these types.
+
+    def default(self, obj):
+        if isinstance(obj, (decimal.Decimal, datetime.date, datetime.datetime)):
+            return str(obj)
+        return GeoJSONEncoder.default(self, obj)
 
 def geojson_renderer_factory(info):
     def _render(value, system):
@@ -6,5 +21,5 @@ def geojson_renderer_factory(info):
         if request is not None:
             if not hasattr(request, 'response_content_type'):
                 request.response_content_type = 'application/json'
-        return geojson.dumps(value)
+        return geojson.dumps(value, cls=Encoder)
     return _render
--- papyrus/tests/test_renderers.py
+++ papyrus/tests/test_renderers.py
@@ -38,3 +38,45 @@ class Test_geojson_renderer_factory(unittest.TestCase):
         result = renderer(f, {'request': request})
         self.assertEqual(result, '{"geometry": {"type": "Point", "coordinates": [53, -4]}, "type": "Feature", "properties": {"title": "Dict 1"}, "id": 1}')
         self.assertEqual(request.response_content_type, 'text/javascript')
+
+    def test_Decimal(self):
+        renderer = self._callFUT(None)
+        import decimal
+        f = {
+            'type': 'Feature',
+            'id': 1,
+            'geometry': {'type': 'Point', 'coordinates': [53, -4]},
+            'properties': {'decimal': decimal.Decimal('0.003')}
+            }
+        request = testing.DummyRequest()
+        result = renderer(f, {'request': request})
+        self.assertEqual(result, '{"geometry": {"type": "Point", "coordinates": [53, -4]}, "type": "Feature", "properties": {"decimal": "0.003"}, "id": 1}')
+        self.assertEqual(request.response_content_type, 'application/json')
+
+    def test_date(self):
+        renderer = self._callFUT(None)
+        import datetime
+        f = {
+            'type': 'Feature',
+            'id': 1,
+            'geometry': {'type': 'Point', 'coordinates': [53, -4]},
+            'properties': {'date': datetime.date(2011, 05, 21)}
+            }
+        request = testing.DummyRequest()
+        result = renderer(f, {'request': request})
+        self.assertEqual(result, '{"geometry": {"type": "Point", "coordinates": [53, -4]}, "type": "Feature", "properties": {"date": "2011-05-21"}, "id": 1}')
+        self.assertEqual(request.response_content_type, 'application/json')
+
+    def test_datetime(self):
+        renderer = self._callFUT(None)
+        import datetime
+        f = {
+            'type': 'Feature',
+            'id': 1,
+            'geometry': {'type': 'Point', 'coordinates': [53, -4]},
+            'properties': {'datetime': datetime.datetime(2011, 05, 21, 20, 55, 12)}
+            }
+        request = testing.DummyRequest()
+        result = renderer(f, {'request': request})
+        self.assertEqual(result, '{"geometry": {"type": "Point", "coordinates": [53, -4]}, "type": "Feature", "properties": {"datetime": "2011-05-21 20:55:12"}, "id": 1}')
+        self.assertEqual(request.response_content_type, 'application/json')
