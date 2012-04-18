722b457f36be6aa070d2ae03d35343b67a7076b0
Éric Lemoine <eric.lemoine@camptocamp.com>
2012-04-18 23:22:07 +0300

    Add shapely mick for RTD.

--- docs/conf.py
+++ docs/conf.py
@@ -218,3 +218,28 @@ man_pages = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# Mocks for Read the Dics
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(self, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['shapely']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
+
