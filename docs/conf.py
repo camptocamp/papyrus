599e2cd1b3116d59bb276cdf1c5ef65e9db90b76
Éric Lemoine <eric.lemoine@camptocamp.com>
2012-04-18 23:29:40 +0300

    Update RTD mock for shapely

--- docs/conf.py
+++ docs/conf.py
@@ -238,7 +238,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['shapely', 'shapely.geometry']
+MOCK_MODULES = ['shapely', 'shapely.geometry', 'shapely.geometry.point', 'shapely.geometry.polygon']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
