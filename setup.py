96be847cb554fcc52f5b773e46ac2f9fa5c9ee00
Éric Lemoine <eric.lemoine@gmail.com>
2012-09-03 09:50:44 +0200

    version is 0.10dev1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.9'
+version = '0.10dev1'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
